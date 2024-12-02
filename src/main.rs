1f81e4c93f433a8f68482209b717a03a02c82f90
Raytesnel <raymondhouwing@gmail.com>
2024-12-02 23:18:43 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use std::fs;
+
+mod day_1;
+
+fn main() {
+    println!("day_1_a answer is: {:?}",day_1::assigment_1_a(read_file("input/assigment_1.txt")));
+    println!("day_1_b answer is: {:?}",day_1::assigment_1_b(read_file("input/assigment_1.txt")));
+}
+
+
+fn read_file(input_path: &str) -> String {
+    //read the file returns a really long string
+    let file_contents = fs::read_to_string(input_path)
+        .expect("LogRocket: Should have been able to read the file{}");
+    return file_contents;
+}
