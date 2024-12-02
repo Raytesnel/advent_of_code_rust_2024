ffeecdbd962500757334f4050ba04a61c644b7a3
Raytesnel <raymondhouwing@gmail.com>
2024-12-02 23:25:24 +0100

    add times

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,12 @@ use std::fs;
 mod day_1;
 
 fn main() {
+    use std::time::Instant;
+    let now = Instant::now();
     println!("day_1_a answer is: {:?}",day_1::assigment_1_a(&read_file("input/assigment_1.txt")));
     println!("day_1_b answer is: {:?}",day_1::assigment_1_b(&read_file("input/assigment_1.txt")));
+    let elapsed = now.elapsed();
+    println!("Elapsed: {:.2?}", elapsed);
 }
 
 
