e191eeaa878f9de4977626ba2ed1bb2d7ccf40a3
raytesnel <raymondhouwing@gmail.com>
2024-12-18 15:56:52 +0100

    setup assignmnet 6

--- src/day_6.rs
+++ src/day_6.rs
@@ -1,38 +1,14 @@
-use crate::day_5::assigment_5_a;
+pub fn assigment_6_a(input_lines: &str)->i32{0}
 
 #[cfg(test)]
 mod tests {
+    use std::fs;
     use super::*;
     #[test]
     fn test_assigment_integration_5_a() {
-        let list_input = "47|53
-97|13
-97|61
-97|47
-75|29
-61|13
-75|53
-29|13
-97|29
-53|29
-61|53
-97|53
-61|29
-47|13
-75|47
-97|75
-47|61
-75|61
-47|29
-75|13
-53|13\r\n\r\n75,47,61,53,29
-97,61,53,29,13
-75,29,13
-75,97,47,61,53
-61,13,29
-97,13,75,29,47";
+        let file_contents = fs::read_to_string("../test_input/assignment_6.txt").expect("couldn't read file");
         let expected_value = 143;
 
-        assert_eq!(assigment_5_a(&list_input), expected_value)
+        assert_eq!(assigment_6_a(&file_contents), expected_value)
     }
 }
